95d50c97b99f5ffcc15d54949f560bece3679db1
Thages W. de Almeida <thageswilson@gmail.com>
2025-08-15 13:17:11 -0300

    Changes to be committed: 	new file:   .gitignore 	modified:   index.html 	new file:   menu-capitao-preview/assets/JS/menuData.js 	new file:   menu-capitao-preview/assets/JS/renderLayout.js 	new file:   menu-capitao-preview/index.html 	new file:   menu-capitao-preview/public/images/banner.jpg 	new file:   menu-capitao-preview/public/images/banner2.jpg 	new file:   menu-capitao-preview/public/images/logo.png 	new file:   menu-capitao-preview/public/images/products/aperol.jpg 	new file:   menu-capitao-preview/public/images/products/black.jpeg 	new file:   menu-capitao-preview/public/images/products/caipirinha.jpg 	new file:   menu-capitao-preview/public/images/products/caipiroska.jpg 	new file:   menu-capitao-preview/public/images/products/carpaccio-carne.jpg 	new file:   menu-capitao-preview/public/images/products/combo-gold.jpeg 	new file:   menu-capitao-preview/public/images/products/combo-red1.webp 	new file:   menu-capitao-preview/public/images/products/combo-red2.webp 	new file:   menu-capitao-preview/public/images/products/ensalada-caesar.jpg 	new file:   menu-capitao-preview/public/images/products/ensalada-capitao.jpg 	new file:   menu-capitao-preview/public/images/products/gin-tonic.jpg 	new file:   menu-capitao-preview/public/images/products/gold.webp 	new file:   menu-capitao-preview/public/images/products/lomito-pasta.webp 	new file:   menu-capitao-preview/public/images/products/lomito.avif 	new file:   menu-capitao-preview/public/images/products/lomo-pasta.jpg 	new file:   menu-capitao-preview/public/images/products/parrillita.jpg 	new file:   menu-capitao-preview/public/images/products/picada-capitao.jpg 	new file:   menu-capitao-preview/public/images/products/picanha-capitao.png 	new file:   menu-capitao-preview/public/images/products/picanha.jpeg 	new file:   menu-capitao-preview/public/images/products/pilsen-long.webp 	new file:   menu-capitao-preview/public/images/products/pilsen.png 	new file:   menu-capitao-preview/public/images/products/rissoto-camarones.jpg 	new file:   menu-capitao-preview/public/images/products/rissoto-funghi.jpeg 	new file:   menu-capitao-preview/public/images/products/rissoto-salmon.jpg 	new file:   menu-capitao-preview/public/images/products/soda-italiana-amostra.jpg 	new file:   menu-capitao-preview/public/images/products/soda-italiana.jpg 	new file:   menu-capitao-preview/public/images/products/tabla-frios.jpg 	new file:   menu-capitao-preview/public/images/products/tostadas.jpg 	new file:   menu-capitao-preview/public/images/products/vaso-caipirinha.jpg 	new file:   menu-capitao-preview/public/images/products/vaso-caipiroska.jpg

---
+++ menu-capitao-preview/assets/JS/menuData.js
@@ -0,0 +1,238 @@
+export const menuData = [
+  {
+    Entradas: {
+      layout: "list",
+      products: [
+        {
+          name: "ENSALADA CAPITÃO",
+          description:
+            "Ensalada compuesta de hortalizas frescas, lechuga americana, tomate cherry, naranja, queso parmesano, huevo cocido, grisines de hojaldre condimentados y almendras con vinagreta.",
+          price: "52000",
+          qty: "",
+          image: "public/images/products/ensalada-capitao.jpg",
+        },
+        {
+          name: "ENSALADA CAESAR",
+          description:
+            "Crujientes hojas de lechuga romana, acompañadas de crotones dorados, lascas de queso parmesano y un aderezo cremoso a base de anchoas.",
+          price: "48000",
+          qty: "",
+          image: "public/images/products/ensalada-caesar.jpg",
+        },
+        {
+          name: "TABLA DE FRÍOS",
+          description:
+            "Mozarella, provolone, parmesano, roquefort, jamón, aceitunas, almendras, pepperoni, huevos de codorniz, salamín, frutillas, uvas, palitos de hojaldre, cherry y mermelada de naranja.",
+          price: "180000",
+          qty: "",
+          image: "public/images/products/tabla-frios.jpg",
+        },
+        {
+          name: "CARPACCIO DE CARNE",
+          description:
+            "Láminas de carne cruda con salsa de alcaparras, mostaza, rúcula y queso; acompañan tostaditas.",
+          price: "85000",
+          qty: "",
+          image: "public/images/products/carpaccio-carne.jpg",
+        },
+        {
+          name: "TOSTADAS",
+          description:
+            "Tostadas de berenjena con queso catupiry y chimichurri.",
+          price: "25000",
+          qty: "",
+          image: "public/images/products/tostadas.jpg",
+        },
+      ],
+      categoryDescription: "",
+      Observations: { text: "", icon: "" },
+    },
+  },
+  {
+    Bebidas: {
+      layout: "list",
+      products: [
+        {
+          name: "JARRA CAIPIRINHA",
+          description: "Jarra 1.000ml",
+          price: "40000",
+          qty: "",
+          image: "public/images/products/caipirinha.jpg",
+        },
+        {
+          name: "JARRA CAIPIROSKA",
+          description: "Jarra 1.000ml",
+          price: "45000",
+          qty: "",
+          image: "public/images/products/caipiroska.jpg",
+        },
+        {
+          name: "VASO CAIPIRINHA",
+          description: "Vaso 300ml",
+          price: "25000",
+          qty: "",
+          image: "public/images/products/vaso-caipirinha.jpg",
+        },
+        {
+          name: "VASO CAIPIROSKA",
+          description: "Vaso 300ml",
+          price: "28000",
+          qty: "",
+          image: "public/images/products/vaso-caipiroska.jpg",
+        },
+        {
+          name: "SODA ITALIANA",
+          description:
+            "Bebida refrescante hecha con agua con gas y jarabe de frutas, servida con hielo.",
+          price: "18000",
+          qty: "",
+          image: "public/images/products/soda-italiana-amostra.jpg",
+        },
+        {
+          name: "GIN TONIC",
+          description:
+            "Cóctel clásico elaborado con ginebra, agua tónica y una rodaja de limón o lima.",
+          price: "25000",
+          qty: "",
+          image: "public/images/products/gin-tonic.jpg",
+        },
+        {
+          name: "APEROL",
+          description:
+            "El Aperol es un aperitivo italiano elaborado con naranja amarga, genciana, ruibarbo y quina.",
+          price: "30000",
+          qty: "",
+          image: "public/images/products/aperol.jpg",
+        },
+        {
+          name: "PILSEN LONG",
+          description: "Long neck 355ml",
+          price: "9000",
+          qty: "",
+          image: "public/images/products/pilsen.png",
+        },
+      ],
+      categoryDescription: "",
+      Observations: { text: "", icon: "" },
+    },
+  },
+  {
+    Combos: {
+      layout: "list",
+      products: [
+        {
+          name: "COMBO RED + 2 AGUA DE COCO",
+          description: "Johnnie Walker Red Label + 2 Agua de Coco",
+          price: "300000",
+          qty: "",
+          image: "public/images/products/combo-red1.webp",
+        },
+        {
+          name: "COMBO RED + 2 ENERGIZANTE",
+          description: "Johnnie Walker Red Label + 2 Energizante",
+          price: "310000",
+          qty: "",
+          image: "public/images/products/combo-red2.webp",
+        },
+        {
+          name: "COMBO GOLD 2 ENERGIZANTE",
+          description: "Johnnie Walker Gold Label + 2 Energizante",
+          price: "990000",
+          qty: "",
+          image: "public/images/products/gold.webp",
+        },
+        {
+          name: "COMBO BLACK + 2 ENERGIZANTE",
+          description: "Johnnie Walker Black Label + 2 Energizante",
+          price: "410000",
+          qty: "",
+          image: "public/images/products/black.jpeg",
+        },
+      ],
+      categoryDescription: "",
+      Observations: { text: "", icon: "" },
+    },
+  },
+  {
+    Picadas: {
+      layout: "list",
+      products: [
+        {
+          name: "DELICIA DE LA CASA",
+          description:
+            "Picada con picaña, pan de ajo y papa frita, acompañada de salsa cuatro quesos.",
+          price: "160000",
+          qty: "",
+          image: "public/images/products/picanha.jpeg",
+        },
+        {
+          name: "PICADA CAPITÃO",
+          description:
+            "Picaña, chorizo toscano, morcilla, butifarra, chorizo con queso, sopa, chipa guazú, pan de ajo y mandioca.",
+          price: "160000",
+          qty: "",
+          image: "public/images/products/picada-capitao.jpg",
+        },
+        {
+          name: "PARRILLITA CAPI",
+          description:
+            "Acompanha picaña, chorizo toscano, morcilla, butifarra, chorizo con queso, sopa, chipa guazu, pan de ajo y mandioca.",
+          price: "160000",
+          qty: "",
+          image: "public/images/products/parrillita.jpg",
+        },
+      ],
+      categoryDescription: "",
+      Observations: { text: "", icon: "" },
+    },
+  },
+  {
+    "A la Carta": {
+      layout: "list",
+      products: [
+        {
+          name: "LOMITO EN SALSA",
+          description:
+            "Lomo con papas a la crema y bastones de polenta crujientes; salsa a elección: pimienta, champignon o Roquefort.",
+          price: "110000",
+          qty: "",
+          image: "public/images/products/lomito.avif",
+        },
+        {
+          name: "RISSOTO AL FUNGHI",
+          description:
+            "Cremoso risotto elaborado con arroz, champiñones (porcini), caldo, vino blanco y queso parmesano.",
+          price: "90000",
+          qty: "",
+          image: "public/images/products/rissoto-funghi.jpeg",
+        },
+        {
+          name: "RISSOTO DE SALMÓN",
+          description:
+            "Cremoso risotto elaborado con arroz arborio, con trozos de salmón ahumado, vino blanco, caldo de verduras, cebolla, ajo, mantequilla y queso parmesano rallado.",
+          price: "110000",
+          qty: "",
+          image: "public/images/products/rissoto-salmon.jpg",
+        },
+        {
+          name: "RISSOTO DE CAMARONES",
+          description:
+            "Cremoso risotto al estilo italiano, preparado con arroz arbóreo, camarones salteados en mantequilla y ajo, terminado con un toque de vino blanco, queso parmesano y finas hierbas. Un plato suave y lleno de sabor marino.",
+          price: "110000",
+          qty: "",
+          image: "public/images/products/rissoto-camarones.jpg",
+        },
+        {
+          name: "LOMITO CON PASTA",
+          description:
+            "Jugoso bife de lomito a la plancha, acompañado de pasta al dente con salsa.",
+          price: "110000",
+          qty: "",
+          image: "public/images/products/lomo-pasta.jpg",
+        },
+      ],
+      categoryDescription: "",
+      Observations: { text: "", icon: "" },
+    },
+  },
+];
